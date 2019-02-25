d3ce927a80a60aee5df86cc408888b12d9a59e33
Mihin Sumaria <mihin.sumaria@gmail.com>
2019-02-24 19:36:50 -0500

    Added additional models

--- models.py
+++ models.py
@@ -2,6 +2,9 @@ from enum import Enum
 
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.linear_model import SGDClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.svm import SVC
+from xgboost import XGBClassifier
 
 
 class LogisticModel(SGDClassifier):
@@ -17,6 +20,12 @@ class LogisticModel(SGDClassifier):
 class Models(Enum):
     logistic = 1
     random_forest = 2
+    xgboost = 3
+    knn = 4
+    svc = 5
 
 Models.logistic.model = LogisticModel
 Models.random_forest.model = RandomForestClassifier
+Models.xgboost.model = XGBClassifier
+Models.knn.model = KNeighborsClassifier
+Models.svc.model = SVC
